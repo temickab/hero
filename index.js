4dede1f7c8e32b0f2bf4eed1ca229489b3735b9b
Temicka Brown <mekapleio@yahoo.com>
2018-02-23 10:17:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const passport = require('passport')
+const morgan = require('morgan')
+const cors = require('cors')
+
+// const articles = require('./controller/articles')
+// const userController = require('./controller/users')
+const methodOverride = require('method-override')
+
+app.use(cors())
+
+app.use('/assets', express.static('assets'))// creating a path for static files had to create an assets file in the html as well as in the css
+app.use(bodyParser.urlencoded({ extended: true }))// for getiing post requestbody
+app.use(methodOverride('_method'))
+
+// require('./config/passport
+
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// The below code is rendering the index.html file
+// Routing
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3001, () => {
+  console.log('hey')
+})
